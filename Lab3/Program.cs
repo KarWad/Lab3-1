10862ab634515f4088415ca51684d0060cc1eb3a
KarWad <phinb@LAPTOP-25LHGEP6.(none)>
2021-03-26 14:50:10 +0100

    Dodaj pliki projektów.

---
+++ Lab3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab3
+{
+    class Program
+
+    {
+        public static Random randoms;
+        static void Main(string[] args)
+        {
+
+            randoms = new Random((int)DateTime.Now.Ticks);
+            var numbers = Enumerable.Range(1, 49).ToList();
+
+            int[] SelectedNumbers = new int[6];
+            
+            for (var i = 0; i < 6; i++) 
+            {
+                var number = GetNumber(numbers.ToArray());
+
+                while (SelectedNumbers.Contains(number)) 
+                {
+                    number = GetNumber(numbers.ToArray());
+                }
+                SelectedNumbers[i] = number;
+            }
+            Console.WriteLine("Lotto numbers: " + string.Join(" ", SelectedNumbers.OrderBy(n => n)));
+        }
+        public static int GetNumber(int[] arr)
+        {
+            if (arr.Length > 1)
+            {
+                var r = randoms.Next(0, arr.Length);
+                var list = arr.ToList();
+                list.RemoveAt(r);
+
+                return GetNumber(list.ToArray());
+            }
+            return arr[0];
+        }
+    }
+}
